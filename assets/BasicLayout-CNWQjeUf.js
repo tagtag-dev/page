dc002ce174ef8e7877d6b0b21b770876a883c740
openwj <openwj@hotmail.com>
2026-03-16 01:12:28 +0800

    update

---
+++ assets/BasicLayout-CNWQjeUf.js
@@ -0,0 +1,1 @@
+import{$i as e,Gi as t,_a as n,pa as r}from"./index-Br9Gio47.js";var i=e({__name:`BasicLayout`,setup(e){return(e,i)=>{let a=n(`router-view`);return r(),t(a)}}});export{i as default};
